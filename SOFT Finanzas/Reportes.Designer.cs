88565caf05c93262b76e8c4b54ce46e04e1692ac
gerson22 <gerson.isaias.rivera@gmail.com>
2015-07-30 21:13:30 -0500

    sistem

--- SOFT Finanzas/Reportes.Designer.cs
+++ SOFT Finanzas/Reportes.Designer.cs
@@ -32,6 +32,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reportes));
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fecha_inicio_report = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // metroLabel1
@@ -49,15 +52,38 @@
             this.notifyIcon1.Text = "Notifiaction";
             this.notifyIcon1.Visible = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SOFT_Finanzas.Properties.Resources.logo_marti;
+            this.pictureBox1.Location = new System.Drawing.Point(174, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(268, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fecha_inicio_report
+            // 
+            this.fecha_inicio_report.AutoSize = true;
+            this.fecha_inicio_report.Location = new System.Drawing.Point(122, 202);
+            this.fecha_inicio_report.Name = "fecha_inicio_report";
+            this.fecha_inicio_report.Size = new System.Drawing.Size(76, 19);
+            this.fecha_inicio_report.TabIndex = 2;
+            this.fecha_inicio_report.Text = "fecha_inicio";
+            this.fecha_inicio_report.Click += new System.EventHandler(this.metroLabel2_Click);
+            // 
             // Reportes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 410);
+            this.ClientSize = new System.Drawing.Size(634, 485);
+            this.Controls.Add(this.fecha_inicio_report);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.metroLabel1);
             this.Name = "Reportes";
             this.Text = "Reportes";
             this.Load += new System.EventHandler(this.Reportes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +93,8 @@
 
         private MetroFramework.Controls.MetroLabel metroLabel1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroLabel fecha_inicio_report;
 
     }
 }
